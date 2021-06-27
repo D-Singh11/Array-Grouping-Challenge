0788c082b88f05ad2b39418b318f48ef0b12f1b9
unknown <sssscorejava@gmail.com>
2021-06-27 23:43:04 +0100

    Implement logic and fix typo

--- src/group-array.js
+++ src/group-array.js
@@ -1,5 +1,16 @@
 const arrayArrayElements = (arr = [], pieces) => {
-  return result;
+    let groupsLeft = pieces;
+    const result = [];
+    let chunkSize;
+    for (let index = 0; index < pieces; index++) {
+        chunkSize = Math.ceil(arr.length / groupsLeft);
+        result.push(arr.slice(0, chunkSize))
+        arr = arr.slice(chunkSize)
+        groupsLeft--;
+    }
+
+    console.log(result);
+    return result;
 };
 
 module.exports = arrayArrayElements;
--- src/group-array.test.js
+++ src/group-array.test.js
@@ -1,16 +1,16 @@
-const arrayArrayElements = require("./array-grouper.js");
+const arrayArrayElements = require("./group-array.js");
 
 test("should split [1, 2, 3, 4, 5] into 3 groups ", () => {
-  result = arrayArrayElements([1, 2, 3, 4, 5], 3);
+    result = arrayArrayElements([1, 2, 3, 4, 5], 3);
 
-  expect(result.length).toEqual(3);
+    expect(result.length).toEqual(3);
 
-  expect(result[0].length).toEqual(2);
-  expect(result[0]).toEqual([1, 2]);
+    expect(result[0].length).toEqual(2);
+    expect(result[0]).toEqual([1, 2]);
 
-  expect(result[1].length).toEqual(2);
-  expect(result[1]).toEqual([3, 4]);
+    expect(result[1].length).toEqual(2);
+    expect(result[1]).toEqual([3, 4]);
 
-  expect(result[2].length).toEqual(1);
-  expect(result[2]).toEqual([5]);
+    expect(result[2].length).toEqual(1);
+    expect(result[2]).toEqual([5]);
 });
